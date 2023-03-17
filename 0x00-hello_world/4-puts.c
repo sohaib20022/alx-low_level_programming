556b9dd8926b14c9e1f3c5f4f60a48945ed160f9
sohaib20022 <sohaibbayou2002@gmail.com>
2023-03-16 18:17:21 -0700

    my commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+* main - Outputs a character string using puts instead of printf.
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	char str[64] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+* main - Outputs a character string using printf instead of puts.
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
