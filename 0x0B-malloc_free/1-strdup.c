f8c64befb40fb399143b0ef8cb24aecc760587d1
sohaib20022 <sohaibbayou2002@gmail.com>
2023-04-11 19:22:44 -0700

    cc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,7 +13,7 @@ char *_strdup(char *str)
         char *aaa;
         int i, r = 0;
 
-        if (str == NUL)
+        if (str == NULL)
                 return (NULL);
         i = 0;
         while (str[i] != '\0')
