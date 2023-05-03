5c09d6924d75dc77dfeb0fd383500fdd240e540a
sohaib20022 <sohaibbayou2002@gmail.com>
2023-05-03 13:59:51 -0700

    r

---
+++ 0x13-more_singly_linked_lists/7-main.c
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    listint_t *node;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    node = get_nodeint_at_index(head, 5);
+    printf("%d\n", node->n);
+    print_listint(head);
+    free_listint2(&head);
+    return (0);
+}
+julien@ubuntu:~/0x13. More singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-main.c 3-add_nodeint_end.c 0-print_listint.c 5-free_listint2.c 7-get_nodeint.c -o h
+julien@ubuntu:~/0x13. More singly linked lists$ ./h 
+0
+1
+2
+3
+4
+98
+402
+1024
+98
+0
+1
+2
+3
+4
+98
+402
+1024
+julien@ubuntu:~/0x13. More singly linked lists$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x13-more_singly_linked_lists
+File: 7-get_nodeint.c
+   
+8. Sum list
+mandatory
+Score: 0.0% (Checks completed: 0.0%)
+Write a function that returns the sum of all the data (n) of a listint_t linked list.
+
+Prototype: int sum_listint(listint_t *head);
+if the list is empty, return 0
+julien@ubuntu:~/0x13. More singly linked lists$ cat 8-main.c 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    int sum;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    sum = sum_listint(head);
+    printf("sum = %d\n", sum);
+    free_listint2(&head);
+    return (0);
+}
